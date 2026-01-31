e5d735de4639989a30f6f858eea1e28d5aacf2b0
Peter Xenopoulos <pnxenopoulos@gmail.com>
2026-01-31 14:39:24 -0500

    restart

--- crates/boon/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Parser module with convenience re-exports
-pub mod parser;
-pub use parser::core::Parser;
-pub use parser::error::ParserError;
-
-// Reader module with convenience re-exports
-pub mod reader;
-pub use reader::bytes::Reader;
-pub use reader::error::ReadError;
